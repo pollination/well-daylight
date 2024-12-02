10624eb42e482c49a0ffa9672ff193a73f929f08
Mikkel Pedersen <mkklpdrsn@gmail.com>
2024-12-02 13:24:47 +0100

    docs(setup): Change recipe icon

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     setup_requires=['setuptools_scm'],
     url='https://github.com/pollination/well-daylight',                   # will be translated to home
     project_urls={
-        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/breeamdaylight4b.png',
+        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/welldaylight.png',
         'docker': 'https://hub.docker.com/r/ladybugtools/honeybee-radiance'
     },
     description='Well daylight recipe for Pollination.',                  # will be used as package description
